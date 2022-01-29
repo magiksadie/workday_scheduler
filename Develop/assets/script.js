378f69d3e2539c36c15ab14e9d3cae11829a9878
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-01-29 13:23:57 -0600

    added HTML for timeblocks

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,4 +1,5 @@
-//Current Date and Time in header
+//Current Date and Time in header using moment.js
 let now = moment().format('MMMM Do YYYY, h:mm:ss a');
-let displayDate =  document.getElementById("currentDay");
+let displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = now;
+
