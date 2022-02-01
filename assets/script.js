3e73bc35a20b74ac5b903ee09f55c66759c85bb9
Sadie <sadiejo.h01@gmail.com>
2022-01-31 19:03:28 -0600

    fixed present class

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,32 @@
-//Current Date and Time in header using moment.js
-let now = moment().format('MMMM Do YYYY, h a');
-let displayDate = document.getElementById("currentDay");
-displayDate.innerHTML = now;
-//Set currentHour
-let currentHour = moment().format('HH');
-console.log(moment().format('HH'));
-
-//Not functioning ):
-//Compare timeblock to current time & change color
-$('.hour').each(function() {
-    var timeRow = $(this).parent().attr("id").split("-")[1];
-    document.getElementsByClassName('past', 'present', 'future');
-//Timeblock == Current time
-    if (currentHour == timeRow) {
-        $(this).children(".description").addClass("present");
-        $(this).addClass('present');
-        //Timeblock > Current Time
-    } else if (currentHour < timeRow) {
-        $(this).removeClass("present");
-        $(this).addClass("future"); 
-        //Timeblock < Current Time
-    } else if (currentHour > timeRow) {
-        $(this).removeClass("future");
-        $(this).addClass("past");
+$(document).ready(function() {
+    //Current Date and Time in header using moment.js
+    let now = moment().format('MMMM Do YYYY, h a');
+    let displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = now;
+    //Set currentHour
+    let currentHour = moment().format('HH');
+    console.log(moment().format('HH'));
+  
+    function setHours() {
+      //Compare timeblock to current time & change color
+      $('.row').each(function() {
+        var timeRow = $(this).attr("id").split("-")[1];
+          //document.getElementsByClassName('.past', '.present', '.future');
+      //Timeblock == Current time
+          if (currentHour == timeRow) {
+              $(this).addClass('present');
+              //Timeblock > Current Time
+          } else if (currentHour < timeRow) {
+              $(this).removeClass('present');
+              $(this).addClass('future'); 
+              //Timeblock < Current Time
+          } else if (currentHour > timeRow) {
+              $(this).removeClass('future');
+              $(this).addClass('past');
+          }
+      });
     }
-});
-
-
-//Save data to local storage
- $(".saveBtn").on('click', function (event) {
-    event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
-    localStorage.setItem(time,value);
-});
-
-//Retrieve data from local storage 
-$("hour0900 .time-block").val(localStorage.getItem("09"));
-$("hour1000 .time-block").val(localStorage.getItem("10"));
-$("hour1100 .time-block").val(localStorage.getItem("11"));
-$("hour1200 .time-block").val(localStorage.getItem("12"));
-$("hour1300 .time-block").val(localStorage.getItem("13"));
-$("hour1400 .time-block").val(localStorage.getItem("14"));
-$("hour1500 .time-block").val(localStorage.getItem("15"));
-$("hour1600 .time-block").val(localStorage.getItem("16"));
-$("hour1700 .time-block").val(localStorage.getItem("17"));
+  
+    setHours();
+  });
 
