1e548dcf320cf94235be0c9d13c3dbf07d609b2c
Sadie <sadiejo.h01@gmail.com>
2022-01-31 19:55:20 -0600

    fixed timeblock past/present/future functionality

--- assets/script.js
+++ assets/script.js
@@ -4,29 +4,42 @@ $(document).ready(function() {
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = now;
     //Set currentHour
-    let currentHour = moment().format('HH');
-    console.log(moment().format('HH'));
+    let currentHour = parseInt(moment().format('HH'));
   
     function setHours() {
-      //Compare timeblock to current time & change color
-      $('.row').each(function() {
-        var timeRow = $(this).attr("id").split("-")[1];
-          //document.getElementsByClassName('.past', '.present', '.future');
-      //Timeblock == Current time
-          if (currentHour == timeRow) {
+        //Compare timeblock to current time & change color
+        $('.row').each(function() {
+          var timeRow = parseInt($(this).attr("id").split("-")[1]);
+          if (currentHour > timeRow) {
+            $(this).addClass('past');
+          } else if (currentHour === timeRow) {
+              $(this).removeClass('past');
               $(this).addClass('present');
-              //Timeblock > Current Time
           } else if (currentHour < timeRow) {
               $(this).removeClass('present');
-              $(this).addClass('future'); 
-              //Timeblock < Current Time
-          } else if (currentHour > timeRow) {
-              $(this).removeClass('future');
-              $(this).addClass('past');
+              $(this).addClass('future');
           }
-      });
-    }
+        });
+      }
   
     setHours();
   });
 
+//Save data to local storage
+ $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time,value);
+});
+
+//Retrieve data from local storage 
+    $("hour-09 .time-block").val(localStorage.getItem("09"));
+    $("hour-10 .time-block").val(localStorage.getItem("10"));
+    $("hour-11 .time-block").val(localStorage.getItem("11"));
+    $("hour-12 .time-block").val(localStorage.getItem("12"));
+    $("hour-13 .time-block").val(localStorage.getItem("13"));
+    $("hour-14 .time-block").val(localStorage.getItem("14"));
+    $("hour-15 .time-block").val(localStorage.getItem("15"));
+    $("hour-16 .time-block").val(localStorage.getItem("16"));
+    $("hour-17 .time-block").val(localStorage.getItem("17")); 
